6b4ee5d846846ce7dd11e2727a36f8a6ff8efb3d
JesusCH92 <jcocaharo@hotmail.com>
2024-01-29 03:18:42 +0100

    implementamos -> puerto de taskTime implementado con doctrine

---
+++ src/TaskTime/Infrastructure/Persistence/DoctrineTaskTimeRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\TaskTime\Infrastructure\Persistence;
+
+use App\Common\Infrastructure\Persistence\DoctrineRepository;
+use App\Task\Domain\Entity\Task;
+use App\TaskTime\Domain\Entity\TaskTime;
+use App\TaskTime\Domain\Repository\TaskTimeRepository;
+
+final class DoctrineTaskTimeRepository extends DoctrineRepository implements TaskTimeRepository
+{
+    public function findUnfinishedTask(Task $task): ?TaskTime
+    {
+        $qb = $this
+            ->ormQueryBuilder()
+            ->select('tt')
+            ->from(TaskTime::class, 'tt')
+            ->join('tt.task', 't')
+            ->andWhere('tt.endDate IS NULL')
+            ->andWhere('t.id = :taskId')
+            ->setParameter('taskId', $task->id());
+
+        return $qb->getQuery()->getOneOrNullResult();
+    }
+
+    public function save(TaskTime $taskTime): void
+    {
+        $this->entityManager()->persist($taskTime);
+        $this->entityManager()->flush();
+    }
+}
