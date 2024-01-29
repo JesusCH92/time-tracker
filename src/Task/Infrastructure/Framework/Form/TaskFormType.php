917801397e630bfd90dfce3600050d95e764ba56
JesusCH92 <jcocaharo@hotmail.com>
2024-01-29 06:25:51 +0100

    implementamos -> creamos un pre submit en el formulario

--- src/Task/Infrastructure/Framework/Form/TaskFormType.php
+++ src/Task/Infrastructure/Framework/Form/TaskFormType.php
@@ -6,6 +6,9 @@ use App\Common\Infrastructure\Framework\Form\Select2FormType;
 use App\Task\Infrastructure\Framework\Form\Model\TaskFormModel;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\RouterInterface;
 
@@ -19,12 +22,34 @@ class TaskFormType extends AbstractType
     {
         $builder->add('taskName', Select2FormType::class, [
             'required' => true,
-            'multiple' => true,
+            'multiple' => false,
             'attr' => [
                 'data-autocomplete-url' => $this->router->generate('app_find_task'),
             ],
             'choices' => []
         ]);
+
+        $formModifier = function (FormInterface $form, ?array $taskName, array $options) {
+            $choices = null === $taskName ? [] : $taskName;
+            $form->add('taskName', Select2FormType::class, [
+                'required' => true,
+                'multiple' => false,
+                'attr' => [
+                    'data-autocomplete-url' => $this->router->generate('app_find_task'),
+                ],
+                'choices' => $choices
+            ]);
+        };
+
+        $builder->addEventListener(
+            FormEvents::PRE_SUBMIT,
+            function (FormEvent $event) use ($formModifier, $options) {
+                $data = $event->getData();
+                $taskName = $data['taskName'] ?? null;
+
+                $formModifier($event->getForm(), $taskName, $options);
+            }
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver)
