57366e6714b7c5c38354199d0650cd7f79e00d98
JesusCH92 <jcocaharo@hotmail.com>
2024-01-29 03:52:02 +0100

    implementamos -> backend componente select2

---
+++ src/Common/Infrastructure/Framework/Form/Select2FormType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Common\Infrastructure\Framework\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Select2FormType extends AbstractType
+{
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $attr = $view->vars['attr'];
+        $class = isset($attr['class']) ? $attr['class'] . ' ' : '';
+        $class .= 'select2';
+        $attr['class'] = $class;
+        $view->vars['attr'] = $attr;
+    }
+
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'attr' => [
+                'class' => 'select2',
+                'data-autocomplete-url' => ''
+            ]
+        ]);
+    }
+}
