fd0a285d2228961852def297e5420fa375ac7cbb
JesusCH92 <jcocaharo@hotmail.com>
2024-01-29 06:23:48 +0100

    implementamos -> implementamos y definimos metodo para obtener la coleccion de task

--- src/Task/Domain/Repository/TaskRepository.php
+++ src/Task/Domain/Repository/TaskRepository.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Task\Domain\Repository;
 
 use App\Task\Domain\Entity\Task;
+use App\Task\Domain\Entity\Tasks;
 
 interface TaskRepository
 {
     public function findById(int $taskId): ?Task;
+
+    public function findAll(string $taskName): Tasks;
 }
--- src/Task/Infrastructure/Persistence/DoctrineTaskRepository.php
+++ src/Task/Infrastructure/Persistence/DoctrineTaskRepository.php
@@ -6,6 +6,7 @@ namespace App\Task\Infrastructure\Persistence;
 
 use App\Common\Infrastructure\Persistence\DoctrineRepository;
 use App\Task\Domain\Entity\Task;
+use App\Task\Domain\Entity\Tasks;
 use App\Task\Domain\Repository\TaskRepository;
 
 final class DoctrineTaskRepository extends DoctrineRepository implements TaskRepository
@@ -13,5 +14,19 @@ final class DoctrineTaskRepository extends DoctrineRepository implements TaskRep
     public function findById(int $taskId): ?Task
     {
         return $this->repository(Task::class)->findOneBy(['id' => $taskId]);
+    }
+
+    public function findAll(string $taskName): Tasks
+    {
+        $qb = $this
+            ->ormQueryBuilder()
+            ->select('t')
+            ->from(Task::class, 't')
+            ->andWhere('t.name LIKE :taskName')
+            ->setParameter('taskName', '%' . $taskName . '%');
+
+        $collection = $qb->getQuery()->getResult();
+
+        return new Tasks($collection);
     }
 }
