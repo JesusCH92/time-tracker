d94be5f76d24ba9c5545c4cb4ec869da4617438c
JesusCH92 <jcocaharo@hotmail.com>
2024-01-29 06:24:57 +0100

    implementamos -> definimos la ruta que obtiene la coleccion de task

--- src/Task/Infrastructure/Controller/TaskController.php
+++ src/Task/Infrastructure/Controller/TaskController.php
@@ -5,15 +5,24 @@ declare(strict_types=1);
 namespace App\Task\Infrastructure\Controller;
 
 use App\Common\Infrastructure\Framework\SymfonyWebController;
+use App\Task\ApplicationService\AllTaskGetter;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class TaskController extends SymfonyWebController
 {
+    public function __construct(private readonly AllTaskGetter $allTaskGetter)
+    {
+    }
+
     #[Route('/task', name: 'app_find_task')]
     public function findTask(Request $request): JsonResponse
     {
-        return new JsonResponse([['id' => 1, 'text' => 'uno'], ['id' => 2, 'text' => 'dos']]);
+        $query = $request->get('q', '');
+
+        $tasks = ($this->allTaskGetter)($query);
+
+        return new JsonResponse($tasks->mappingDropdown());
     }
 }
